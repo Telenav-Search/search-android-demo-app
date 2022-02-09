46c46522c2d1a3f5cba0dcb025250ffbe772495b
Aram, Baghdasaryan <baghdasaryana@telenav.com>
2022-02-09 11:55:14 +0400

    Move user address UI to seperate fragment

--- app/src/main/java/telenav/demo/app/personalinfo/PersonalInfoFragment.kt
+++ app/src/main/java/telenav/demo/app/personalinfo/PersonalInfoFragment.kt
@@ -9,14 +9,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.telenav.sdk.datacollector.api.DataCollectorService
-import com.telenav.sdk.datacollector.model.event.EntityCacheActionEvent
 import com.telenav.sdk.entity.model.base.Entity
-import com.telenav.sdk.entity.model.base.EntityType
 import telenav.demo.app.R
 import telenav.demo.app.databinding.FragmentPersonalInfoBottomBinding
-import telenav.demo.app.entitydetails.EntityDetailsActivity
 import telenav.demo.app.utils.deleteFavorite
-import telenav.demo.app.utils.entityCachedClick
 import java.lang.reflect.Type
 import androidx.recyclerview.widget.ItemTouchHelper
 import telenav.demo.app.utils.SwipeToDeleteCallback
@@ -45,8 +41,6 @@ class PersonalInfoFragment : RoundedBottomSheetLayoutNew() {
         binding?.personalInfoOta?.setOnClickListener { showHomeAreaActivity() }
 
         getFavoriteData()
-        fillHomeInfo()
-        fillWorkInfo()
     }
 
     private fun getFavoriteData() {
@@ -86,76 +80,6 @@ class PersonalInfoFragment : RoundedBottomSheetLayoutNew() {
     private fun deleteFavoriteEntity(entity: Entity) {
         dataCollectorClient.deleteFavorite(requireContext(), entity)
         getFavoriteData()
-    }
-
-    private fun fillHomeInfo() {
-        val prefs =
-            requireContext().getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
-        val storedHome = Gson().fromJson(
-            prefs.getString(getString(R.string.saved_home_address_key), ""),
-            Entity::class.java
-        )
-
-        if (storedHome == null) {
-            binding?.homeAddressConfigure?.text = requireContext().getString(R.string.setup)
-            return
-        } else {
-            binding?.homeAddressConfigure?.text = requireContext().getString(R.string.edit)
-        }
-
-        val name =
-            if (storedHome.type == EntityType.ADDRESS) storedHome.address.formattedAddress else storedHome.place.name
-
-        binding?.homeAddress?.text = name
-        binding?.homeAddress?.setOnClickListener {
-            dataCollectorClient.entityCachedClick(storedHome.id, EntityCacheActionEvent.SourceType.HOME)
-            startActivity(
-                Intent(
-                    requireContext(),
-                    EntityDetailsActivity::class.java
-                ).apply {
-                    putExtra(EntityDetailsActivity.PARAM_ID, storedHome.id)
-                    putExtra(
-                        EntityDetailsActivity.PARAM_SOURCE,
-                        EntityCacheActionEvent.SourceType.HOME.name
-                    )
-                })
-        }
-    }
-
-    private fun fillWorkInfo() {
-        val prefs =
-            requireContext().getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
-        val storedWork = Gson().fromJson(
-            prefs.getString(getString(R.string.saved_work_address_key), ""),
-            Entity::class.java
-        )
-
-        if (storedWork == null) {
-            binding?.workAddressConfigure?.text = requireContext().getString(R.string.setup)
-            return
-        } else {
-            binding?.workAddressConfigure?.text = requireContext().getString(R.string.edit)
-        }
-
-        val name =
-            if (storedWork.type == EntityType.ADDRESS) storedWork.address.formattedAddress else storedWork.place.name
-
-        binding?.workAddress?.text = name
-        binding?.workAddress?.setOnClickListener {
-            dataCollectorClient.entityCachedClick(storedWork.id, EntityCacheActionEvent.SourceType.WORK)
-            startActivity(
-                Intent(
-                    requireContext(),
-                    EntityDetailsActivity::class.java
-                ).apply {
-                    putExtra(EntityDetailsActivity.PARAM_ID, storedWork.id)
-                    putExtra(
-                        EntityDetailsActivity.PARAM_SOURCE,
-                        EntityCacheActionEvent.SourceType.WORK.name
-                    )
-                })
-        }
     }
 
     private fun showHomeAreaActivity() {
