9f53d9f9d83ea6014879ea61de03da33208d227c
VegeDoggie <athlon7@126.com>
2022-05-18 14:43:42 +0800

    Remove useless elements and code.

--- app/src/main/java/telenav/demo/app/settings/SettingsActivity.kt
+++ app/src/main/java/telenav/demo/app/settings/SettingsActivity.kt
@@ -6,24 +6,24 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.RadioButton
-import android.widget.SeekBar
-import android.widget.SeekBar.OnSeekBarChangeListener
 import android.widget.TextView
+
 import androidx.appcompat.app.AppCompatActivity
-import com.telenav.sdk.core.SDKRuntime
-import ir.androidexception.filepicker.dialog.DirectoryPickerDialog
+
 import kotlinx.android.synthetic.main.activity_settings.*
+
+import ir.androidexception.filepicker.dialog.DirectoryPickerDialog
+
+import com.telenav.sdk.core.SDKRuntime
+
 import telenav.demo.app.App
 import telenav.demo.app.R
 import telenav.demo.app.initialization.SearchMode
 import telenav.demo.app.initialization.saveIndexDataPath
 import telenav.demo.app.initialization.saveSearchMode
 import telenav.demo.app.map.MapActivity.Companion.IS_ENV_CHANGED
-import telenav.demo.app.search.PREDICTIONS_LIMIT_DEF
-import telenav.demo.app.search.SEARCH_LIMIT_WITH_FILTERS
-import telenav.demo.app.search.SUGGESTIONS_LIMIT_DEF
-import java.io.File
 
+import java.io.File
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -52,42 +52,6 @@ class SettingsActivity : AppCompatActivity() {
 
         findViewById<View>(R.id.settings_back).setOnClickListener { save() }
         findViewById<View>(R.id.settings_select_index_data_path).setOnClickListener { openDirectoryForIndex() }
-        search_bar.progress = App.readStringFromSharedPreferences(App.SEARCH_LIMIT,
-                SEARCH_LIMIT_WITH_FILTERS.toString())!!.toInt()
-        count_search.text = App.readStringFromSharedPreferences(App.SEARCH_LIMIT,
-                SEARCH_LIMIT_WITH_FILTERS.toString())
-        search_bar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-                count_search.text = progress.toString()
-            }
-
-            override fun onStartTrackingTouch(seekBar: SeekBar) {}
-            override fun onStopTrackingTouch(seekBar: SeekBar) {}
-        })
-        sugg_bar.progress = App.readStringFromSharedPreferences(App.SUGGESTIONS_LIMIT,
-                SUGGESTIONS_LIMIT_DEF.toString())!!.toInt()
-        count_sugg.text = App.readStringFromSharedPreferences(App.SUGGESTIONS_LIMIT,
-                SUGGESTIONS_LIMIT_DEF.toString())
-        sugg_bar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-                count_sugg.text = progress.toString()
-            }
-
-            override fun onStartTrackingTouch(seekBar: SeekBar) {}
-            override fun onStopTrackingTouch(seekBar: SeekBar) {}
-        })
-        predictions_bar.progress = App.readStringFromSharedPreferences(App.PREDICTIONS_LIMIT,
-                PREDICTIONS_LIMIT_DEF.toString())!!.toInt()
-        count_predictions.text = App.readStringFromSharedPreferences(App.PREDICTIONS_LIMIT,
-                PREDICTIONS_LIMIT_DEF.toString())
-        predictions_bar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-                count_predictions.text = progress.toString()
-            }
-
-            override fun onStartTrackingTouch(seekBar: SeekBar) {}
-            override fun onStopTrackingTouch(seekBar: SeekBar) {}
-        })
         environment = App.readStringFromSharedPreferences(App.ENVIRONMENT,
                 "0")!!.toInt()
         val adapter: ArrayAdapter<*> = ArrayAdapter.createFromResource(this, R.array.env, android.R.layout.simple_spinner_item)
@@ -148,24 +112,9 @@ class SettingsActivity : AppCompatActivity() {
         searchMode = SearchMode.fromInt(mode)
     }
 
-    private fun saveLimits() {
-        App.writeStringToSharedPreferences(App.SEARCH_LIMIT,
-                search_bar.progress.toString())
-        if (search_bar.progress < App.readFromSharedPreferences(App.FILTER_NUMBER)) {
-            App.writeToSharedPreferences(
-                    App.FILTER_NUMBER,
-                    search_bar.progress)
-        }
-        App.writeStringToSharedPreferences(App.SUGGESTIONS_LIMIT,
-                sugg_bar.progress.toString())
-        App.writeStringToSharedPreferences(App.PREDICTIONS_LIMIT,
-                predictions_bar.progress.toString())
-    }
-
     private fun save() {
         saveIndexDataPath(indexDataPath)
         saveSearchMode(searchMode)
-        saveLimits()
         SDKRuntime.setNetworkAvailable(searchMode == SearchMode.HYBRID)
         if (spinner.selectedItemPosition != environment) {
             App.writeStringToSharedPreferences(App.ENVIRONMENT,
